5c7aadbbf18f2531f14e88a08c800737e2704683
Atharv Yadav <atharvyadav96k@gmail.com>
2025-10-01 18:15:56 +0530

    command line argument support

--- Main.java
+++ Main.java
@@ -2,12 +2,9 @@ import Evalaturs.Evaluate;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        Path filePath = Paths.get(str);
+        Path filePath = Paths.get(args[0]);
         try {
             String code = new String(Files.readAllBytes(filePath));
             new Evaluate(code);
